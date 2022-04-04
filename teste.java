b5bd9664aee72c25e8bb5492ee2bb0680010299d
Eduardo <eduardo.trigueiro@hotmail.com>
2022-04-04 10:41:44 -0300

    Adicionando arquivo teste em Java

---
+++ teste.java
@@ -0,0 +1,8 @@
+public class teste{
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+        System.out.println(2+4);
+        System.out.println(2*7);
+        
+    }
+}
