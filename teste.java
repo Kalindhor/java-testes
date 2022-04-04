08b347b4bd7e065b70560deeda626f98e1009223
Eduardo <eduardo.trigueiro@hotmail.com>
2022-04-04 10:55:58 -0300

    Adicionando comentários do que foi aprendido na aula

--- teste.java
+++ teste.java
@@ -6,3 +6,13 @@ public class teste{
         
     }
 }
+
+
+
+
+//O que foi aprendido
+//Comentários // -> são úteis para indicar comentários/notas no projetos
+//Ponto de entrada - entry-point -> é o ponto onde todos sistemass procuram no seu projeto para iniciar
+//Metodos: são instruções que define para o progama executar - Exemplo:
+//Instrução chamda System.out.println("")
+//propriedades/variaveis -> será onde iremos armazenar dados para manipularmos no futuro
