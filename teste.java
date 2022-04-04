ff9d4adf6aff5b3aefb4689629af9bb152de94fd
Eduardo <eduardo.trigueiro@hotmail.com>
2022-04-04 11:39:35 -0300

    Aprendendo sobre variaveis

--- teste.java
+++ teste.java
@@ -1,18 +1,24 @@
 public class teste{
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
-        System.out.println(2+4);
-        System.out.println(2*7);
+        var login = "Olá mundo";
+        var number1 = 199;
         
+
+        //Variaveis tipo : textos (strings)
+        //Variaveis tipo: numeros (int)
+        //Variaveis tipo: Numeros fracionados (Double)
+        //Variaveis tipo: boolean (Variaveis|Falso)
+
+        //Tipos de Dado
+
+
+
+
+        System.out.println(login);
+        System.out.println(number1);
     }
 }
 
 
 
 
-//O que foi aprendido
-//Comentários // -> são úteis para indicar comentários/notas no projetos
-//Ponto de entrada - entry-point -> é o ponto onde todos sistemass procuram no seu projeto para iniciar
-//Metodos: são instruções que define para o progama executar - Exemplo:
-//Instrução chamda System.out.println("")
-//propriedades/variaveis -> será onde iremos armazenar dados para manipularmos no futuro
